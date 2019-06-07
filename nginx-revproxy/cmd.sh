384ee778c7e6f08c36226faad809bb7cfe6e2d44
Isami Yuma <lemonburst1958@gmail.com>
2019-06-07 20:54:29 +0900

    Test let's encrypt

--- nginx-revproxy/cmd.sh
+++ nginx-revproxy/cmd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# certbot-auto --nginx -d matsura-yuma.site -d www.matsura-yuma.site -m lemonburst1958@gmail.com --agree-tos -n
-certbot-auto certonly --standalone -d matsura-yuma.site -d www.matsura-yuma.site -m lemonburst1958@gmail.com --agree-tos -n
+certbot-auto --nginx -d matsura-yuma.site -d www.matsura-yuma.site -m lemonburst1958@gmail.com --agree-tos -n
+# certbot-auto certonly --standalone -d matsura-yuma.site -d www.matsura-yuma.site -m lemonburst1958@gmail.com --agree-tos -n
 certbot-auto renew
